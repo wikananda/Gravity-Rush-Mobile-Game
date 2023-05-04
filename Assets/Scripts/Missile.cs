59affe5151e4ddaf0e3db0af1f271eedc4e96d77
Komang Wikananda <nandasantana276@gmail.com>
2023-05-04 23:33:21 +0700

    Fixed missile unable to despawn

--- Assets/Scripts/Missile.cs
+++ Assets/Scripts/Missile.cs
@@ -5,7 +5,17 @@ using UnityEngine;
 public class Missile : MonoBehaviour
 {
     float speed = 1f;
-    private void FixedUpdate() {
+    private void FixedUpdate() 
+    {
         transform.position = Vector3.Lerp(transform.position, transform.position + Vector3.left * speed, 0.2f);
+    }
+
+     private void OnCollisionEnter2D(Collision2D collision) 
+    {
+        if (collision.gameObject.tag == "Despawn")
+        {
+            Destroy(gameObject);
+            Debug.Log("Missile Despawned");
+        }
     }
 }
