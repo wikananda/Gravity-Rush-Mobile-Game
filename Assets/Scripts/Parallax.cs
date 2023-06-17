eb242ac4bce7d6246cd8c1d9126afaa3d0e57b95
Komang Wikananda <nandasantana276@gmail.com>
2023-06-17 22:05:52 +0700

    Made item object move along parallax - Despawn for item still bugging

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -6,13 +6,17 @@ public class Parallax : MonoBehaviour
 {
     private MeshRenderer meshRenderer;
     public float parallaxDepth = 1f;
+    public bool useTexture = false;
 
     float initialSpeed;
     float parallaxSpeed;
     
     Player player;
     private void Awake() {
-        meshRenderer = GetComponent<MeshRenderer>();
+        if (useTexture)
+        {
+            meshRenderer = GetComponent<MeshRenderer>();
+        }
         player = GameObject.Find("Player").GetComponent<Player>();
         initialSpeed = player.speed;
     }
@@ -21,6 +25,23 @@ public class Parallax : MonoBehaviour
     void Update()
     {
         parallaxSpeed = player.speed * parallaxDepth / (10 * initialSpeed);
-        meshRenderer.material.mainTextureOffset += new Vector2(parallaxSpeed * Time.deltaTime, 0);
+        if (useTexture)
+        {
+            meshRenderer.material.mainTextureOffset += new Vector2(parallaxSpeed * Time.deltaTime, 0);
+        }
+        else
+        {
+            transform.position = new Vector3(transform.position.x - player.speed * Time.deltaTime, transform.position.y, transform.position.z);
+        }
+    }
+
+    // Trigger still bugging for item, maybe because item object's collider also "is trigger"
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.gameObject.tag == "Despawn")
+        {
+            Debug.Log("Item despawned");
+            Destroy(gameObject);
+        }
     }
 }
